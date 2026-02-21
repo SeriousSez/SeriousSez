dfe53340c2b141909c3d76ce401b33db1edf2d2d
Sezgin Sahin <moyumbnm@hotmail.com>
2026-02-21 20:55:50 +0100

    aaa

--- SeriousSez.Api/Controllers/HealthController.cs
+++ SeriousSez.Api/Controllers/HealthController.cs
@@ -3,11 +3,17 @@
 namespace SeriousSez.Api.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
-    public class HealthController : ControllerBase
+    [Route("[controller]")]
+    public class HealthController
     {
         [HttpGet("ping")]
         public IActionResult Ping()
+        {
+            return new OkResult();
+        }
+
+        [HttpGet("ping2")]
+        public IActionResult Ping2()
         {
             return new OkResult();
         }
