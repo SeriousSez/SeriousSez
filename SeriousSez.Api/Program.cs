9d15010b238c20ecd6c601478dafa2231be3b083
Sezgin Sahin <moyumbnm@hotmail.com>
2026-02-22 21:39:30 +0100

    a

--- SeriousSez.Api/Program.cs
+++ SeriousSez.Api/Program.cs
@@ -5,6 +5,8 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using SeriousSez.Infrastructure;
 using System;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace SeriousSez
 {
@@ -12,11 +14,57 @@ namespace SeriousSez
     {
         public static void Main(string[] args)
         {
-            var host = CreateHostBuilder(args).Build();
+            RegisterGlobalExceptionHandlers();
 
-            CreateDbIfNotExists(host);
+            try
+            {
+                var host = CreateHostBuilder(args).Build();
 
-            host.Run();
+                CreateDbIfNotExists(host);
+
+                host.Run();
+            }
+            catch (Exception ex)
+            {
+                TryWriteFatalStartupLog("Fatal exception during application startup.", ex);
+                throw;
+            }
+        }
+
+        private static void RegisterGlobalExceptionHandlers()
+        {
+            AppDomain.CurrentDomain.UnhandledException += (_, eventArgs) =>
+            {
+                var exception = eventArgs.ExceptionObject as Exception;
+                TryWriteFatalStartupLog("Unhandled exception.", exception);
+            };
+
+            TaskScheduler.UnobservedTaskException += (_, eventArgs) =>
+            {
+                TryWriteFatalStartupLog("Unobserved task exception.", eventArgs.Exception);
+            };
+        }
+
+        private static void TryWriteFatalStartupLog(string message, Exception exception)
+        {
+            try
+            {
+                var baseDirectory = AppContext.BaseDirectory;
+                var logsDirectory = Path.Combine(baseDirectory, "logs");
+                Directory.CreateDirectory(logsDirectory);
+
+                var timestamp = DateTime.UtcNow.ToString("yyyy-MM-ddTHH-mm-ssZ");
+                var logPath = Path.Combine(logsDirectory, $"startup-fatal-{timestamp}.log");
+
+                var content = $"[{DateTime.UtcNow:O}] {message}{Environment.NewLine}{exception}";
+                File.WriteAllText(logPath, content);
+
+                var latestLogPath = Path.Combine(logsDirectory, "startup-fatal-latest.log");
+                File.WriteAllText(latestLogPath, content);
+            }
+            catch
+            {
+            }
         }
 
         private static void CreateDbIfNotExists(IHost host)
